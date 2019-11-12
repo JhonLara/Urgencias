ee0f440b787f23e8e4af26ed928755e89318ca5a
Jhon Lara <jhon.lara@ceiba.com.co>
2019-11-12 11:57:16 -0500

    Se ajusta dependencias para los test

--- Urgencias-Servicio/src/test/java/com/ceiba/urgencias/UrgenciasApplicationTests.java
+++ Urgencias-Servicio/src/test/java/com/ceiba/urgencias/UrgenciasApplicationTests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
