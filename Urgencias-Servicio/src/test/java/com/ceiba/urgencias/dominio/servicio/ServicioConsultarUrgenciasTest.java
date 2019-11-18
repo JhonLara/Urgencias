25501bbcab3b9852e3da454e3df9427aa4082244
Jhon Lara <jhon.lara@ceiba.com.co>
2019-11-17 19:41:13 -0500

    Se añaden pruebas unitarias

---
+++ Urgencias-Servicio/src/test/java/com/ceiba/urgencias/dominio/servicio/ServicioConsultarUrgenciasTest.java
@@ -0,0 +1,36 @@
+package com.ceiba.urgencias.dominio.servicio;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.ceiba.urgencias.dominio.modelo.Urgencia;
+import com.ceiba.urgencias.dominio.puerto.repositorio.RepositorioUrgencia;
+
+public class ServicioConsultarUrgenciasTest {
+
+	@Test
+	public void ValorHabitacionConDescuento() {
+
+		Urgencia urgencia = new Urgencia(100L, "Carlos", LocalDate.now(), "SURA", LocalDate.now(), LocalDate.now());
+		List<Urgencia> listaUrgencias = new ArrayList<>();
+		listaUrgencias.add(urgencia);
+		ServicioConsultarUrgencias servicioConsultarUrgencias = Mockito.mock(ServicioConsultarUrgencias.class);
+		Mockito.when(servicioConsultarUrgencias.ejecutar()).thenReturn(listaUrgencias);
+
+		RepositorioUrgencia repositorioUrgencia = Mockito.mock(RepositorioUrgencia.class);
+		Mockito.when(repositorioUrgencia.obtenerUrgencias()).thenReturn(listaUrgencias);
+
+		List<Urgencia> urgenciasObtenidas = servicioConsultarUrgencias.ejecutar();
+
+		// assert
+		Assertions.assertNotNull(urgenciasObtenidas);
+		Assertions.assertNotNull(urgenciasObtenidas.get(0));
+		Assertions.assertEquals(100L, urgenciasObtenidas.get(0).getIdPaciente());
+
+	}
+}
